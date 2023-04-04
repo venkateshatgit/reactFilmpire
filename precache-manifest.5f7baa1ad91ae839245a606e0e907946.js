063e63fa2c8dfdd583f103b20e6e3eab0f7e7d92
Gunake <venkatesh.gunake@capgemini.com>
2023-04-04 12:38:29 +0530

    Updates

---
+++ precache-manifest.5f7baa1ad91ae839245a606e0e907946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b5bb6809f36f161700a",
+    "url": "/reactFilmpire/static/css/main.da97cdfc.chunk.css"
+  },
+  {
+    "revision": "6b5bb6809f36f161700a",
+    "url": "/reactFilmpire/static/js/main.6b5bb680.chunk.js"
+  },
+  {
+    "revision": "315327e41e41ef1b3c48",
+    "url": "/reactFilmpire/static/js/1.315327e4.chunk.js"
+  },
+  {
+    "revision": "8b47722ee53f514a5e58",
+    "url": "/reactFilmpire/static/js/2.8b47722e.chunk.js"
+  },
+  {
+    "revision": "cb04a41efcd15c10e933",
+    "url": "/reactFilmpire/static/js/runtime~main.cb04a41e.js"
+  },
+  {
+    "revision": "22f7bc01a9efcbbaff29df63905dcbe4",
+    "url": "/reactFilmpire/index.html"
+  }
+];
